4bce10ad00248a5f03df84e11cc07e4bed54470d
jzinn1997 <jz460@georgetown.edu>
2019-02-13 10:27:48 -0500

    top sellers loop

--- monthly_sales.py
+++ monthly_sales.py
@@ -8,7 +8,9 @@ import pandas
 def to_usd(my_price):
     return "${0:,.2f}".format(my_price) 
 
+#
 #INPUTS
+#
 
 csv_filename = "sales-201803.csv" #must allow user to specify
 csv_filepath = os.path.join(os.path.dirname(__file__), "data", csv_filename)
@@ -16,6 +18,8 @@ csv_data = pandas.read_csv(csv_filepath)
 
 #print(type(cvs_data))
 #print(list(csv_data.columns))
+# print(list(csv_data.columns))
+
 
 #
 #CALCULATIONS
@@ -44,11 +48,33 @@ print(product_totals)
  #   print(product_name)
     # breakpoint()
 
-top_sellers = [
-    {"rank": 1, "name": "Button-Down Shirt", "monthly_sales": 6960.35},
-    {"rank": 2, "name": "Super Soft Hoodie", "monthly_sales": 1875.00},
-]
-# get above information from csv_data
+#top_sellers = [
+#    {"rank": 1, "name": "Button-Down Shirt", "monthly_sales": 6960.35},
+#    {"rank": 2, "name": "Super Soft Hoodie", "monthly_sales": 1875.00},
+#]
+## get above information from csv_data
+
+#rank = 1
+#top_sellers = []
+#product_names = product_totals.index.values.tolist()
+#for product_name in product_names:
+#    monthly_sales = 10
+#    top_sellers.append({"rank": rank, "name": product_name, "monthly_sales": monthly_sales})
+#    rank = rank + 1
+top sellers = []
+
+rank = 1
+
+for i, row in product_totals.iterrows():
+    #breakpoint()
+    #print(type(row))
+    #print(row.name)
+    #print(row["sales price"])
+    d = {"rank": rank, "name": row.name, "monthly_sales": row["sales price"]}
+    top_sellers.append(d)
+    rank = rank + 1
+
+
 
 
 
@@ -68,6 +94,7 @@ print("-----------------------")
 print("TOP SELLING PRODUCTS:")
 for d in top_sellers:
     print("  " + str(d["rank"]) + ") " + d["name"] + ": " + to_usd(d["monthly_sales"]))
+
 print("-----------------------")
 print("VISUALIZING THE DATA...")
 
