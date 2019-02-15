8ed3d254da010f820e203393520e09ff38abb1c9
jzinn1997 <jz460@georgetown.edu>
2019-02-15 17:01:30 -0500

    cleaning code

--- monthly_sales.py
+++ monthly_sales.py
@@ -5,10 +5,9 @@ import os
 import pandas
 
 import matplotlib.pyplot as plt
-
 import matplotlib.ticker as ticker
 
-
+#function from https://github.com/s2t2/shopping-cart-screencast/blob/30c2a2873a796b8766e9b9ae57a2764725ccc793/shopping_cart.py#L56-L59
 def to_usd(my_price):
     return "${0:,.2f}".format(my_price) 
 
@@ -17,10 +16,12 @@ def to_usd(my_price):
 #
 
 csv_filename = "sales-201803.csv" #TODO: must allow user to specify
-csv_filepath = os.path.join(os.path.dirname(__file__), "data", csv_filename)
-csv_data = pandas.read_csv(csv_filepath)
 
-#original formulas found on prof rossetti github
+#referenced https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/notes/python/modules/os.md#file-operations
+csv_filepath = os.path.join(os.path.dirname(__file__), "data", csv_filename)
+
+#referenced https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/notes/python/packages/pandas.md
+csv_data = pandas.read_csv(csv_filepath)
 
 #
 #CALCULATIONS
@@ -29,34 +30,10 @@ csv_data = pandas.read_csv(csv_filepath)
 #setting variables
 
 monthly_total = csv_data["sales price"].sum()
-#product_totals = csv_data.groupby(["product"]).sum()
-#product_totals = product_totals.sort_values("sales price", ascending=False)
-
-#this was using plotly
-#top_sellers = []
-#rank = 1
-#for i, row in product_totals.iterrows():
-#    d = {"rank": rank, "name": row.name, "monthly_sales": row["sales price"]}
-#    top_sellers.append(d)
-#    rank = rank + 1
-
-#switching to matplotlib
 
 product_names = csv_data["product"]
-
 unique_product_names = product_names.unique()
-
 unique_product_names = unique_product_names.tolist()
-
-#top_sellers = [
-#    {"name": "Button-Down Shirt", "monthly_sales": 6960.34},
-#    {"name": "Super Soft Hoodie", "monthly_sales": 1875.0},
-#    {"name": "Khaki Pants", "monthly_sales": 1602.0},
-#    {"name": "Vintage Logo Tee", "monthly_sales": 941.05},
-#    {"name": "Brown Boots", "monthly_sales": 250.0},
-#    {"name": "Sticker Pack", "monthly_sales": 216.0},
-#    {"name": "Baseball Cap", "monthly_sales": 156.31}   
-#]
 
 top_sellers = []
 
@@ -93,31 +70,6 @@ print("-----------------------")
 print("VISUALIZING THE DATA...")
 
 
-#chart_filename = "top-sellers-201803.html" 
-#chart_filepath = os.path.join(os.path.dirname(__file__), "reports", chart_filename)
-
-#image_filename = "top-sellers-201803.png" # TODO: parse selected csv file name
-#image_filepath = os.path.join(os.path.dirname(__file__), "images", image_filename) # looks like the image just gets downloaded
-#image_filename = "top-sellers-201803" # TODO: parse selected csv file name
-
-
-
-#original code founded here: https://plot.ly/python/bar-charts/
-#breakpoint()
-#sorted_sales = [d["monthly_sales"] for d in top_sellers]   
-#sorted_names = [d["name"] for d in top_sellers]
-#
-#x=sorted_sales
-#y=sorted_names
-
-#data = [
-#    graph_objs.Bar(
-#        x=x,
-#        y=y,
-#        orientation = 'h' 
-#    )
-#]
-
 chart_title = "Top Selling Products (February 2019)" # TODO: get month and year
 
 sorted_names = []
@@ -127,7 +79,7 @@ for d in top_sellers:
     sorted_names.append(d["name"])
     sorted_sales.append(d["monthly_sales"])
 
-
+#display top-sellers at top
 sorted_names.reverse()
 sorted_sales.reverse()
 
@@ -136,41 +88,13 @@ fig, ax = plt.subplots()
 usd_formatter = ticker.FormatStrFormatter('$%1.0f')
 ax.xaxis.set_major_formatter(usd_formatter)
 
+#nuts and bolts of the chart
 plt.barh(sorted_names, sorted_sales)
 plt.title(chart_title)
 plt.xlabel("Product")
 plt.ylabel("Monthly Sales (USD)")
 
-#trying to fix labels getting cut off
+#fixes labels getting cut off
 plt.tight_layout()
 plt.show()
-
-
-
-#orginal code founded here: https://plot.ly/python/getting-started/
-
-# layout = graph_objs.Layout(
-    #title=chart_title,
-    #yaxis=dict(autorange="reversed") # make sure top sellers are on the top
-# 
-# 
-# chart_options = {"data": data, "layout": layout}
-# 
-# chart_options = {
-#    "data": data,
-#    "layout": graph_objs.Layout(title="Top Selling Products (February 2019)")
-# 
-# plotly.offline.plot(chart_options, auto_open=True)
-# 
-# 
-# plotly.offline.plot(
-#    chart_options,
-#    filename=chart_filepath,
-#    #image="png",
-#    #image_filename=image_filename, #specify filepath???????
-#    auto_open=True
-# 
-
-#print("-----------------------")
-#print("SEE: " + chart_filepath)
 
